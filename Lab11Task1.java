e7b7462d95daa93d4995f623584197af133b3ce0
kjaaron <132103589+kjaaron@users.noreply.github.com>
2023-04-28 17:57:01 -0400

    Add files via upload

---
+++ Lab11Task1.java
@@ -0,0 +1,11 @@
+public class Lab11Task1 //Kojo Addo, 11-8-22
+{
+ public static void main(String[] args)
+ {  
+    String[] names = {"Einstein", "Newton", "Copernicus", "Kepler"};
+    
+    for( int index=0; index<4;index++)
+    System.out.println("Element at index " + index + " is: " + names[index]);
+ }
+  
+}
